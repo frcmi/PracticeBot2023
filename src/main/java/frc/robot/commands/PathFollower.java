222e6b964fe0590782400445b18fa357e1e75cf5
Jonas Brastad <jonas.brastad@misd400.org>
2023-03-09 17:17:30 -0800

    Added wrapper, couldnt test it  sorry

---
+++ src/main/java/frc/robot/commands/PathFollower.java
@@ -0,0 +1,79 @@
+package frc.robot.commands;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPRamseteCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class PathFollower extends CommandBase {
+    private DriveSubsystem drivetrain;
+    private PathPlannerTrajectory path;
+    private boolean firstPath;
+
+    private PathPlannerTrajectory pathPlannerTrajectory;
+    private CommandBase pathCommand;
+
+    public PathFollower(DriveSubsystem drivetrain, String path, PathConstraints constraints, boolean firstPath) {
+        this.drivetrain = drivetrain;
+        this.firstPath = firstPath;
+
+        pathPlannerTrajectory = PathPlanner.loadPath(path, constraints);
+
+        addRequirements(drivetrain);
+    }
+    public PathFollower(DriveSubsystem drivetrain, PathPlannerTrajectory path, boolean firstPath) {
+        this.drivetrain = drivetrain;
+        this.firstPath = firstPath;
+
+        pathPlannerTrajectory = path;
+        
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        if(firstPath)
+            drivetrain.resetOdometry(path.getInitialHolonomicPose());
+        
+        pathCommand = new PPRamseteCommand(
+            pathPlannerTrajectory, 
+            drivetrain::getPose,
+            new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+            new SimpleMotorFeedforward(
+                DriveConstants.ksVolts,
+                DriveConstants.kvVoltSecondsPerMeter,
+                DriveConstants.kaVoltSecondsSquaredPerMeter),
+            DriveConstants.kDriveKinematics,
+            drivetrain::getWheelSpeeds,
+            new PIDController(AutoConstants.kTurnP, 0, 0),
+            new PIDController(AutoConstants.kTurnP, 0, 0),
+            // RamseteCommand passes volts to the callback
+            drivetrain::tankDriveVolts, // Voltage biconsumer
+            true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+            drivetrain // Requires this drive subsystem   
+        ); 
+    }
+
+    @Override
+    public void execute() {
+        pathCommand.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        pathCommand.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pathCommand.isFinished();
+    }
+}
